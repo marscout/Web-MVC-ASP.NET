fee9f31e77999ae3e510a11068ed2fa8b3cb2f02
Marcelo Rondini <marcelo-salvioni@hotmail.com>
2019-04-26 22:37:44 -0300

    Seeding Service

---
+++ CursoWebMVC/Data/CursoWebMVCContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoWebMVC.Models
+{
+    public class CursoWebMVCContext : DbContext
+    {
+        public CursoWebMVCContext (DbContextOptions<CursoWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SelesRecords{ get; set; }
+    }
+}
