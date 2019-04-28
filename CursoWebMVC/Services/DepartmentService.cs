117cdfc949571967d1638f4b01d751277c760c2e
Marcelo Rondini <marcelo-salvioni@hotmail.com>
2019-04-28 19:34:54 -0300

    Adicionado o componente de departamento em ViewSeller

---
+++ CursoWebMVC/Services/DepartmentService.cs
@@ -0,0 +1,23 @@
+﻿using CursoWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoWebMVC.Services
+{
+    public class DepartmentService
+    {
+        private readonly CursoWebMVCContext _context;
+
+        public DepartmentService(CursoWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
