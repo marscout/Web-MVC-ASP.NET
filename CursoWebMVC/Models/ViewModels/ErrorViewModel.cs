040068e66a80db01184525e6de2d7118841b5311
Marcelo Rondini <marcelo-salvioni@hotmail.com>
2019-04-29 15:32:43 -0300

    Formatacao e localidade ajustada

--- CursoWebMVC/Models/ViewModels/ErrorViewModel.cs
+++ CursoWebMVC/Models/ViewModels/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace CursoWebMVC.Models.ErrorModel
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
