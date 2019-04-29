b45f6c4198334d3a136dad4123faf8d06074782e
Marcelo Rondini <marcelo-salvioni@hotmail.com>
2019-04-29 12:32:24 -0300

    Tela de detalhe

--- CursoWebMVC/Services/SellerService.cs
+++ CursoWebMVC/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using CursoWebMVC.Services.Exceptions;
 
 namespace CursoWebMVC.Services
 {
@@ -37,6 +38,22 @@ namespace CursoWebMVC.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update (Seller seller)
+        {
+            if (!_context.Seller.Any(x => x.Id == seller.Id))
+            {
+                throw new NotFoundException("Id Not Found");
+            }
+            try { 
+            _context.Update(seller);
+            _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
